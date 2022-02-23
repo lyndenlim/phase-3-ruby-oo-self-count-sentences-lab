9a7aa53b0ca760f886bbe60c75f427ca8a86582a
Lynden Lim <lyndenlim15@gmail.com>
2022-02-23 11:45:11 -0500

    commit tree 179dbccb2a44fdd3da0ae5acd6553b65be0e7c63

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,36 @@
-require 'pry'
+require "pry"
 
 class String
-
   def sentence?
-    
+    if self[self.length - 1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self[self.length - 1] == "?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[self.length - 1] == "!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    if self.length > 0
+      sentences = self.split(/[.!?]/).filter { |element| element != "" }
+      return sentences.count
+    else
+      return 0
+    end
   end
 end
